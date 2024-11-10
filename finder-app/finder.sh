24b2757013d06f782a618f01be5c077d66b8beca
avt82 <tm.software@gmail.com>
2024-11-10 02:13:33 +0100

    Assignment 3 part 2 is done

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,70 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-function my_error()
-{
-    # maybe we want some logging functionality later?
-    echo "${1}"
+if [ "$#" != "2" ] ; then
+    echo "Not enough arguments"
     exit 1
-}
+fi
 
-function my_success()
-{
-    if [ ! -z "${1}" ] ; then
-	echo "${1}"
-    fi
-    exit 0
-}
+if [ ! -d "${1}" ] ; then
+    echo "Directory \"${1}\" does not exist" ; 
+    exit 1
+fi
 
-cnt_files=0
-
-function my_find()
-{
-    a=`find "${1}"`
-
-    if [ -z "${a}" ] ; then
-	echo "<nothing found>"
-    fi
-
-    echo "------------------------------------"
-    echo " File list in directory \"${1}\":"
-    echo "------------------------------------"
-    while read -r line ; do
-	# we don't care for empty lines
-	if [ -z "${line}" ] ; then continue; fi
-	# we don't care about "."
-	if [ "${line}" == "." ] ; then continue; fi
-	# we don't care about empty dirs
-	if [ ! -f "${line}" ]  ; then continue; fi
-	# print file name - we can catch it in the caller
-	echo " -> ${line}"
-	(( cnt_files++ ))
-    done <<< "${a}"
-    echo "Total: ${cnt_files} file(s)"
-}
+# my_find $1
+cnt_files=`find "${1}" | wc -l`
+if [ "${cnt_files}" != 0 ] ; then
+    $(( cnt_files = cnt_files - 1 )) 2>/dev/null
+fi
 
 cnt_grep=0
+a=`grep -Hrn "${2}" "${1}" `
+cnt_grep=`echo "${a}" | wc -l`
+echo "------------------------------------"
+echo " Found ${cnt_grep} occurence(s) of \"${2}\""
+echo "------------------------------------"
+echo "${a}"
+echo "------------------------------------"
 
-function my_grep()
-{
-    a=`grep -Hrn "${2}" "${1}"`
-    cnt_grep=`echo "${a}" | wc -l`
-    echo "------------------------------------"
-    echo " Found ${cnt_grep} occurence(s) of \"${2}\""
-    echo "------------------------------------"
-    echo "${a}"
-    echo "------------------------------------"
-}
+echo "The number of files are ${cnt_files} and the number of matching lines are ${cnt_grep}"
 
-function my_summup()
-{
-    echo "The number of files are ${cnt_files} and the number of matching lines are ${cnt_grep}"
-}
-
-if [ "$#" != "2" ] ; then my_error "Not enough arguments" ; fi
-if [ ! -d "${1}" ] ; then my_error "Directory \"${1}\" does not exist" ; fi
-
-my_find "${1}"
-my_grep "${1}" "${2}"
-my_summup
-
-my_success "Ok"
+exit 0
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,47 +34,108 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make     ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make     ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+fi
+
+if [ ! -f "${OUTDIR}/Image" ] ; then
+    sudo cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 fi
 
 echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
+if [ -d "${OUTDIR}/rootfs" ] ; then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sys sbin tmp
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+mkdir -p conf
 
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ] ; then
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+libs_output=`${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep -e "program interpreter" -e "Shared librar"`
 
-# TODO: Make device nodes
+libs_list=
+# I'm too lazy to do it manually...
+while read -r line ; do
+    line=`echo "${line}" | cut -d] -f1`
+    tmp=`echo "${line}" | cut -d[ -f2`
+    if [ ! -z "${tmp}" ] ; then line="${tmp}" ; fi
+    tmp=`echo "${line}" | cut -d: -f2`
+    if [ ! -z "${tmp}" ] ; then line="${tmp}" ; fi
 
-# TODO: Clean and build the writer utility
+    while [ true ] ; do
+	tmp=`echo "${line}" | cut -d/ -f1`
+	if [ "${tmp}" == "${line}" ] ; then break ; fi
+	if [ -z "${tmp}" ] ; then break ; fi
+	line="${line:${#tmp}+1}"
+    done
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+    libs_list="${libs_list} ${line}"
+done <<< "${libs_output}"
+
+sysroot=`${CROSS_COMPILE}gcc -print-sysroot`
+
+# copying the libraries
+for thelib in ${libs_list} ; do
+    src=`find ${sysroot} -name ${thelib} | tail -n 1`
+    dst="lib"
+    if [ ! -z `echo "${src}" | grep "lib64"` ] ; then dst="${dst}64" ; fi
+    echo "Copying ${thelib} to ${dst}..."
+    cp ${src} ${OUTDIR}/rootfs/${dst}/.
+done
+
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
+
+# Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} all
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp    writer ${OUTDIR}/rootfs/home/.
+cp    *.sh   ${OUTDIR}/rootfs/home/.
+cp -r conf   ${OUTDIR}/rootfs/home/.
+cp -r conf/* ${OUTDIR}/rootfs/conf/.
 
-# TODO: Chown the root directory
+# Chown the root directory
+cd ${OUTDIR}
+sudo chown -R root:root rootfs
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+echo "Find $?"
+cd ${OUTDIR}
+gzip -f ${OUTDIR}/initramfs.cpio
+echo "GZip $?"
